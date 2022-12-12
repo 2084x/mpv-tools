e186db3a54707a0110c38a13f87bfbe42595ab48
Jameson <108906685+2084x@users.noreply.github.com>
2022-12-12 19:18:39 +1100

    simplify dox

--- auto-set.lua
+++ auto-set.lua
@@ -10,9 +10,7 @@ local opts = {
 
     	-- Ignore script if string matches path.
     	ignore = {
-		-- "://",	-- Never auto-loop network paths.
-		-- "%.jpg$",    -- Never auto-pause jpg files.
-    		-- "/pix/",	-- Ignore script when playing files in the /pix/ directory.
+        	"://",
 		},
 
 }
@@ -23,30 +21,26 @@ local function auto_set()
     	local path = mp.get_property_native("path")
     	local duration = mp.get_property_native("duration")
 
-    	-- Checks if script should be ignored.
     	for x,pattern in pairs(opts.ignore) do
         	if path:find(pattern) then
             		return
         	end
     	end
 
-    	-- Pauses if path has no duration.
     	if not duration then
         	mp.set_property_native("pause", true)
-		mp.msg.log("info", "file paused")
+		print("file paused")
         	return
     	end
 
-    	-- Gets loop status.
     	local looping = mp.get_property_native("loop-file")
-    	-- Enables looping.
+
     	if not looping and duration <= opts.length then
         	mp.set_property_native("loop-file", true)
-		mp.msg.log("info", "looping enabled")
-    	-- Disables looping.
+		print("looping enabled")
     	elseif looping and duration > opts.length then
         	mp.set_property_native("loop-file", false)
-		mp.msg.log("info", "looping disabled")
+		print("looping disabled")
     	end
 end
 
