f24ec39afab53a06595653db0f05152ffdf487c6
Jameson <108906685+2084x@users.noreply.github.com>
2022-12-12 19:51:11 +1100

    change name, messages and variables

--- rs-toggle.lua
+++ rs-toggle.lua
@@ -1,31 +1,30 @@
 -- Toggle redshift when using mpv.
 -- Assumes that redshift is running.
 
-rs_enabled = true
-active = false
+rs_on = true
+on = false
 
 function rs_toggle()
 	os.execute("pkill -x -USR1 redshift")
 end
 
 function turn_on()
-
-    	active = true
-    	mp.osd_message("Redshift toggling activated")
+    	on = true
+    	mp.osd_message("[rs-toggle] on")
 
     	function rs_disable()
-        	if rs_enabled then
+        	if rs_on then
             		rs_toggle()
-            		rs_enabled = false
-            		mp.msg.log("info", "Disabling redshift")
+            		rs_on = false
+            		print("disabling redshift")
         	end
     	end
 
     	function rs_enable()
-        	if not rs_enabled then
+        	if not rs_on then
             		rs_toggle()
-            		rs_enabled = true
-            		mp.msg.log("info", "Reenabling redshift")
+            		rs_on = true
+            		print("reenabling redshift")
         	end
     	end
 
@@ -38,7 +37,7 @@ function turn_on()
     	end
 
     	function on_pause_change(name, value)
-        	if value then  --pause started
+        	if value then
             		rs_enable()
         	else
             		rs_disable()
@@ -48,28 +47,25 @@ function turn_on()
     	mp.register_event("file-loaded", rs_handler)
     	mp.register_event("shutdown", rs_enable)
     	mp.observe_property("pause", "bool", on_pause_change)
-
 end
 
 function turn_off()
+	on = false
+    	mp.osd_message("[rs-toggle] off")
 
-	active = false
-    	mp.osd_message("Redshift toggling deactivated")
-
-    	if not rs_enabled then
+    	if not rs_on then
         	rs_toggle()
-        	rs_enabled = true
-        	mp.msg.log("info", "Reenabling redshift")
+        	rs_on = true
+        	print("reenabling redshift")
     	end
 
     	mp.unregister_event(rs_handler)
     	mp.unregister_event(rs_enable)
     	mp.unobserve_property(on_pause_change)
-
 end
 
 function toggle()
-    	if active then
+    	if on then
         	turn_off()
     	else
         	turn_on()
