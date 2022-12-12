9714d841c7ff9a6f64b20bed03d5882ba689be21
Jameson <108906685+2084x@users.noreply.github.com>
2022-12-12 19:27:01 +1100

    properly remove ass

--- blackout-next.lua
+++ blackout-next.lua
@@ -2,34 +2,28 @@
 
 assdraw = require "mp.assdraw"
 
-active = false
+on = false
 
 function toggle()
-
-	if not active then
-		active = true
-		dim = 1
+	if not on then
+		on = true
 		mp.set_property_native("mute", true)
             	mp.commandv("script-message", "osc-visibility", "never", "no-osd")
-	elseif active then
-		active = false
-		dim = 0
+		local ass = assdraw.ass_new()
+        	local w, h = mp.get_osd_size()
+        	local alpha = 255 - math.ceil(255 * 1)
+        	ass.text = string.format("{\\pos(0,0)\\rDefault\\an7\\1c&H000000&\\alpha&H%X&}", alpha)
+        	ass:draw_start()
+        	ass:rect_cw(0, 0, w, h)
+        	ass:draw_stop()
+        	ass:new_event()
+        	mp.set_osd_ass(w, h, ass.text)
+	elseif on then
+		on = false
 		mp.set_property_native("mute", false)
-		mp.set_property_native("pause", false)
             	mp.commandv("script-message", "osc-visibility", "auto", "no-osd")
+        	mp.set_osd_ass(0, 0, "")
 	end
-
-	local ass = assdraw.ass_new()
-        local w, h = mp.get_osd_size()
-        local alpha = 255 - math.ceil(255 * dim)
-
-        ass.text = string.format("{\\pos(0,0)\\rDefault\\an7\\1c&H000000&\\alpha&H%X&}", alpha)
-        ass:draw_start()
-        ass:rect_cw(0, 0, w, h)
-        ass:draw_stop()
-        ass:new_event()
-
-        mp.set_osd_ass(w, h, ass.text)
 end
 
 mp.add_key_binding("b", "blackout", toggle)
